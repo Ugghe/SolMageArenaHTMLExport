e60230e43217ff27c8ded228868a7b022e5c0ce7
Ugghe <E_G182@yahoo.com>
2022-01-17 00:52:46 -0800

    Read from Phantom

---
+++ phantom/script.js
@@ -0,0 +1,48 @@
+// import loadMetadata from "../src";
+
+let walletAddress;
+let connection;
+let web3 = solanaWeb3;
+let tokenAccounts = [];
+
+//Game Variables
+let walletLoaded = false
+
+window.onload = async () => {
+
+    try {
+        connection = await window.solana.connect();
+        walletAddress = connection.publicKey.toString();
+        console.log("Success....");
+        console.log(walletAddress);
+    } catch (err) {
+        console.log("Catch Block Ran");
+        console.error(err);
+        console.log("Inside script.js");
+    }
+
+    // console.log("web3 bundle", web3.Connection);
+
+    connection = new web3.Connection(
+        "https://crimson-spring-silence.solana-mainnet.quiknode.pro/ace9fd07b18929f276b9537eba5260c15e9703ff/",
+        'confirmed',
+    )
+
+    tokenAccounts = await connection.getParsedTokenAccountsByOwner(
+        new web3.PublicKey(walletAddress),
+        { programId: new web3.PublicKey("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA") }
+    );
+
+    console.log("Parsed Token Accounts", tokenAccounts);
+
+    window.metadata = await window.loadMetadata(tokenAccounts, connection);
+
+
+    console.log("MetaData log: ", metadata[0].name);
+    console.log("MetaData log: ", metadata[0].attributes);
+    console.log("MetaData Size: ", metadata.length);
+
+    console.log("Attribute: ", metadata[0].attributes[0].value)
+    walletLoaded = true;
+}
+
